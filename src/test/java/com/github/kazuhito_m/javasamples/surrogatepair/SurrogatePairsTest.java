22122f57bbe55523a4e4558f3b04def76255cd45
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-11-02 17:21:08 +0900

    修正。パッケージをつけたのと、参考にした記事をコメントに張った。

--- src/test/java/com/github/kazuhito_m/javasamples/surrogatepair/SurrogatePairsTest.java
+++ src/test/java/com/github/kazuhito_m/javasamples/surrogatepair/SurrogatePairsTest.java
@@ -1,3 +1,5 @@
+package com.github.kazuhito_m.javasamples.surrogatepair;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -5,10 +7,19 @@ import org.slf4j.LoggerFactory;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * http://www.kanazawa-net.ne.jp/~pmansato/others/other_surrogate.htm
+ * http://blog.unfindable.net/archives/3406
+ * https://www.ibm.com/developerworks/jp/ysl/library/java/j-unicode_surrogate/index.html
+ */
 public class SurrogatePairsTest {
 
     private static final Logger log = LoggerFactory.getLogger(SurrogatePairsTest.class);
 
+    /**
+     * https://kadoppe.com/archives/2011/03/java-string-char-conversio.html
+     * https://qiita.com/msakamoto_sf/items/bdc62f95f6d342705001
+     */
     @Test
     public void 文字コード指定でサロゲートペア文字列を作成する() {
         // サカナ偏に花
@@ -23,6 +34,10 @@ public class SurrogatePairsTest {
         log.info(surrogatePair);
     }
 
+    /**
+     * http://blog.sarabande.jp/post/103624680728
+     * https://diary.sshida.com/20150317-19-サロゲートペア文字の例%20-%20%26#x20bb7; (U%2B20b
+     */
     @Test
     public void コードポイントからサロゲートペア文字を作成する() {
         String surrogatePair = "𩸽";
